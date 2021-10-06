4e4886c095aa2772bca07f9c6a748b3dd644c0c4
parkGunWoong <bung1438@gmail.com>
2021-10-07 02:59:51 +0900

    주요 속성을 이해하기 위한 두 번째 튜토리얼 실습

--- es6-modules/webpack.config.js
+++ es6-modules/webpack.config.js
@@ -2,11 +2,17 @@ var path = require('path');
 
 module.exports = {
 	mode: 'production',
+
+	// 주요속성 1) 빌드를 할 대상 파일을 지정
 	entry: './js/app.js',
+
+	// 주요속성 2) 빌드를 하고 나서 즉 웹팩으로 변환을 하고 나서 나오는 결과물을 정의하는 경로
 	output: {
-			path: path.resolve(__dirname, 'build'),
-			filename: 'main.bundle.js'
+		path: path.resolve(__dirname, 'build'),
+		filename: 'main.bundle.js'
 	},
+
+	// 주요속성 3) entry -> output으로 변환할 때 중간에 개입하는 부분
 	module: {
 		rules: [{
 			test: /\.m?js$/,
@@ -20,7 +26,7 @@ module.exports = {
 		}]
 	},
 	stats: {
-			colors: true
+		colors: true
 	},
-	devtool: 'source-map'
+	devtool: 'source-map'	// 개발자 도구의 소스맵 기능을 활용할 수 있게 된다.
 };
