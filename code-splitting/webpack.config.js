65a0484c2421c6825aed05e8c96836b49514225b
parkGunWoong <bung1438@gmail.com>
2021-10-07 04:01:05 +0900

    웹팩 설정 파일 분석

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -3,7 +3,7 @@ var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'none', // production, development, none
-  entry: './index.js', 
+  entry: './index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
